6fb4023318e874ad6742182feaddc289987c1553
Vishnu <vishnu.kp@dicortech.com>
2023-01-27 10:07:03 +0530

    A comment is addded tried to checkout from branch without commiting then stashed it and switched branch then switch back and

--- add.c
+++ add.c
@@ -5,12 +5,12 @@ int sum (int x, int y);
 void main()
 {
 	int num1, num2;
-	printf("Sum of %d and %d = %d", 
-			num1, num2, sum(num1, num2));
+	printf("Sum = %d\n\r", sum(num1, num2));
 	
 }
 
 int sum (int x, int y)
+//functon to find the sum of 2 numbers
 {
 	printf("Enter the First Number : ");
 	scanf("%d", &x);
